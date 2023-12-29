19d23a582156fb6121be95b5b6da6b07a207e322
maxwill <0312fs3@gmail.com>
2023-12-29 15:59:27 -0500

    complete sort gadget and filter using linear time verifiable sort. todo: packaging for easier test case switch and write up

--- plonky2/examples/filter_perm.rs
+++ plonky2/examples/filter_perm.rs
@@ -1,15 +1,17 @@
 use anyhow::Result;
+// use itertools::izip;
 use log::LevelFilter;
 use plonky2::field::types::Field;
-use plonky2::iop::challenger::RecursiveChallenger;
-use plonky2::iop::target::BoolTarget;
+use plonky2::iop::target::Target;
 use plonky2::iop::witness::{PartialWitness, WitnessWrite};
 use plonky2::plonk::circuit_builder::CircuitBuilder;
 use plonky2::plonk::circuit_data::CircuitConfig;
 use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
 // use serde::de::value::BoolDeserializer;
 
-const N: usize = 100;
+const N: usize = 5;
+const N_LOG: usize = 7;
+const N_BIT: usize = 16;
 
 fn main() -> Result<()> {
     env_logger::Builder::new()
@@ -24,78 +26,102 @@ fn main() -> Result<()> {
 
     // The arithmetic circuit.
     let query = builder.add_virtual_target();
-    let initial_keys = builder.add_virtual_targets(N);
-    let initial_vals = builder.add_virtual_targets(N);
-    let mut matches = initial_keys
+    let keys = builder.add_virtual_targets(N);
+    let vals = builder.add_virtual_targets(N);
+    let matches = keys
         .iter()
         .map(|k| builder.is_equal(*k, query).target)
         .collect::<Vec<_>>();
+    // let zero_target = builder.zero();
 
     // get num matches
     let num_matches = matches
         .iter()
         .fold(builder.zero(), |acc, m| builder.add(acc, *m));
 
-    // shift n times
-    let mut vals = initial_vals.clone();
-    let zero = builder.zero();
-    for _ in 0..(N - 1) {
-        (0..N).fold(builder._false(), |moving, i| {
-            let not_match = builder.not(BoolTarget::new_unsafe(matches[i]));
-            let moving = builder.or(moving, not_match);
-            let next_match = if i == N - 1 { zero } else { matches[i + 1] };
-            let next_val = if i == N - 1 { zero } else { vals[i + 1] };
-            matches[i] = builder._if(moving, next_match, matches[i]);
-            vals[i] = builder._if(moving, next_val, vals[i]);
-            moving
-        });
-    }
-    let final_keys = matches
+    // encode the tuples
+    let shift_match_target = builder.constant(F::from_canonical_u64(1 << (N_BIT + N_LOG)));
+    let shift_index_target = builder.constant(F::from_canonical_u64(1 << (N_BIT)));
+    let compact = matches
+        .iter()
+        .zip(vals.iter())
+        .enumerate()
+        .map(|(i, (m, v))| {
+            let shifted_match = builder.mul(*m, shift_match_target);
+            let index = builder.constant(F::from_canonical_usize(N - i));
+            let shifted_index_add_val = builder.mul_add(index, shift_index_target, *v);
+            builder.add(shifted_match, shifted_index_add_val)
+        })
+        .collect::<Vec<_>>();
+
+    // sort the encoded inputs
+    let sorted_compact: Vec<Target> =
+        builder.sort::<<C as GenericConfig<D>>::Hasher>(&compact, 1 + N_BIT + N_LOG);
+
+    // decode the inputs
+    let sorted_matches = sorted_compact
+        .iter()
+        .map(|c| {
+            let bits = builder.split_le(*c, 1 + N_BIT + N_LOG);
+            bits[N_BIT + N_LOG].target
+        })
+        .collect::<Vec<_>>();
+    let sorted_keys = sorted_matches
         .iter()
         .map(|m| builder.mul(*m, query))
+        .collect::<Vec<_>>();
+    let sorted_vals = sorted_compact
+        .iter()
+        .zip(sorted_matches.iter())
+        .map(|(c, m)| {
+            // this is a convenient way to get the low bits
+            // we can also do bit composition only and call `builder.le_sum` since we only need to get low bits
+            let (low, _) = builder.split_low_high(*c, N_BIT, 64);
+            builder.mul(low, *m)
+        })
         .collect::<Vec<_>>();
 
     // public inputs
     builder.register_public_input(query);
-    builder.register_public_inputs(&initial_keys);
-    builder.register_public_inputs(&initial_vals);
+    builder.register_public_inputs(&keys);
+    builder.register_public_inputs(&vals);
     let num_public_inputs = 1 + 2 * N;
     // generated public outpus
     builder.register_public_input(num_matches);
-    builder.register_public_inputs(&final_keys);
-    builder.register_public_inputs(&vals);
+    builder.register_public_inputs(&sorted_keys);
+    builder.register_public_inputs(&sorted_vals);
 
     // print stats
     builder.print_gate_counts(0);
 
     let mut pw = PartialWitness::new();
-    pw.set_target(query, F::from_canonical_usize(N - 1));
-    pw.set_target_arr(
-        &initial_keys,
-        &(0..N)
-            .map(|i| F::from_canonical_usize(i))
-            .collect::<Vec<_>>(),
-    );
-    pw.set_target_arr(
-        &initial_vals,
-        &(0..N)
-            .map(|i| F::from_canonical_usize(i + N))
-            .collect::<Vec<_>>(),
-    );
-    // pw.set_target(query, F::from_canonical_usize(2));
+    // pw.set_target(query, F::from_canonical_usize(N - 1));
     // pw.set_target_arr(
-    //     &initial_keys,
-    //     &[2, 0, 2, 1, 2]
-    //         .iter()
-    //         .map(|i| F::from_canonical_usize(*i))
+    //     &keys,
+    //     &(0..N)
+    //         .map(|i| F::from_canonical_usize(i))
     //         .collect::<Vec<_>>(),
     // );
     // pw.set_target_arr(
-    //     &initial_vals,
+    //     &vals,
     //     &(0..N)
     //         .map(|i| F::from_canonical_usize(i + N))
     //         .collect::<Vec<_>>(),
     // );
+    pw.set_target(query, F::from_canonical_usize(2));
+    pw.set_target_arr(
+        &keys,
+        &[2, 0, 2, 1, 2]
+            .iter()
+            .map(|i| F::from_canonical_usize(*i))
+            .collect::<Vec<_>>(),
+    );
+    pw.set_target_arr(
+        &vals,
+        &(0..N)
+            .map(|i| F::from_canonical_usize(i + N))
+            .collect::<Vec<_>>(),
+    );
 
     let data = builder.build::<C>();
     let proof = data.prove(pw)?;
--- plonky2/src/gadgets/permutation_check.rs
+++ plonky2/src/gadgets/permutation_check.rs
@@ -1,11 +1,52 @@
 use crate::field::extension::Extendable;
 use crate::hash::hash_types::RichField;
 use crate::iop::challenger::RecursiveChallenger;
+use crate::iop::generator::{GeneratedValues, SimpleGenerator};
 use crate::iop::target::Target;
+use crate::iop::witness::{PartitionWitness, Witness, WitnessWrite};
 use crate::plonk::circuit_builder::CircuitBuilder;
+use crate::plonk::circuit_data::CommonCircuitData;
 use crate::plonk::config::AlgebraicHasher;
+use crate::util::serialization::{Buffer, IoResult, Read, Write};
 
 impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
+    // sort an array a, generator required
+    pub fn sort<H: AlgebraicHasher<F>>(
+        &mut self,
+        original_array: &[Target],
+        n_log: usize,
+    ) -> Vec<Target> {
+        let sorted_array = self.add_virtual_targets(original_array.len());
+        self.sorted_check::<H>(&sorted_array, original_array, n_log);
+        self.add_simple_generator(SortGenerator {
+            original_array: original_array.to_vec(),
+            sorted_array: sorted_array.clone(),
+        });
+        sorted_array
+    }
+
+    // verify that a < b when both are under n_log bits
+    pub fn assert_leq(&mut self, smaller: Target, larger: Target, n_log: usize) {
+        // (2^16 - 1) - 0 < 2^16
+        let diff = self.sub(larger, smaller);
+        self.range_check(diff, n_log)
+    }
+
+    // verify that a is a sorted version of b
+    pub fn sorted_check<H: AlgebraicHasher<F>>(
+        &mut self,
+        sorted_array: &[Target],
+        original_array: &[Target],
+        n_log: usize,
+    ) {
+        // check: a is sorted
+        for i in 1..sorted_array.len() {
+            self.assert_leq(sorted_array[i], sorted_array[i - 1], n_log);
+        }
+        // check: a is a permutation of b
+        self.permutation_check::<H>(sorted_array, original_array);
+    }
+
     // verify that a is a permutation of b
     pub fn permutation_check<H: AlgebraicHasher<F>>(&mut self, a: &[Target], b: &[Target]) {
         let mut challenger = RecursiveChallenger::<F, H, D>::new(self);
@@ -21,6 +62,53 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
             self.mul(acc, term)
         });
         self.connect(a_eval, b_eval);
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct SortGenerator {
+    original_array: Vec<Target>,
+    sorted_array: Vec<Target>,
+}
+
+impl<F: RichField + Extendable<D>, const D: usize> SimpleGenerator<F, D> for SortGenerator {
+    fn id(&self) -> String {
+        "SortGenerator".to_string()
+    }
+
+    fn dependencies(&self) -> Vec<Target> {
+        self.original_array.clone()
+    }
+
+    fn run_once(&self, witness: &PartitionWitness<F>, out_buffer: &mut GeneratedValues<F>) {
+        let mut array = witness
+            .get_targets(&self.original_array)
+            .into_iter()
+            .map(|x| x.to_canonical_u64() as usize)
+            .collect::<Vec<_>>();
+        array.sort();
+        array.reverse();
+        out_buffer.set_target_arr(
+            &self.sorted_array,
+            &array
+                .into_iter()
+                .map(|x| F::from_canonical_usize(x))
+                .collect::<Vec<_>>(),
+        );
+    }
+
+    fn serialize(&self, dst: &mut Vec<u8>, _common_data: &CommonCircuitData<F, D>) -> IoResult<()> {
+        dst.write_target_vec(&self.original_array)?;
+        dst.write_target_vec(&self.sorted_array)
+    }
+
+    fn deserialize(src: &mut Buffer, _common_data: &CommonCircuitData<F, D>) -> IoResult<Self> {
+        let original_array = src.read_target_vec()?;
+        let sorted_array = src.read_target_vec()?;
+        Ok(Self {
+            original_array,
+            sorted_array,
+        })
     }
 }
 
@@ -78,6 +166,49 @@ mod tests {
 
         let data = builder.build::<C>();
         let proof = data.prove(pw)?;
+
+        verify(proof, &data.verifier_only, &data.common)
+    }
+
+    #[test]
+    fn test_sort() -> Result<()> {
+        env_logger::Builder::new()
+            .filter_level(LevelFilter::Debug)
+            .init();
+
+        const D: usize = 2;
+        type C = PoseidonGoldilocksConfig;
+        type F = <C as GenericConfig<D>>::F;
+        // type FF = <C as GenericConfig<D>>::FE;
+        const N: usize = 100;
+        const K: usize = 3;
+
+        let config = CircuitConfig::standard_recursion_config();
+
+        let mut pw = PartialWitness::<F>::new();
+        let mut builder = CircuitBuilder::<F, D>::new(config);
+
+        let a = builder.add_virtual_targets(N);
+        let b = builder.sort::<<C as GenericConfig<D>>::Hasher>(&a, 16);
+
+        // builder.register_public_inputs(&a);
+        builder.register_public_inputs(&b);
+
+        let a_vals = (0..N)
+            .map(|i| F::from_canonical_usize((i + K) % N))
+            .collect::<Vec<_>>();
+
+        println!("a_vals: {:?}", a_vals);
+
+        pw.set_target_arr(&a, &a_vals);
+
+        // print stats
+        builder.print_gate_counts(0);
+
+        let data = builder.build::<C>();
+        let proof = data.prove(pw)?;
+
+        println!("sorted_a: {:?}", proof.public_inputs);
 
         verify(proof, &data.verifier_only, &data.common)
     }
